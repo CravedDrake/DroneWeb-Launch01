d5007951403078be45636fad3022dfa6514badf0
sunim thapa <thapasunim@gmail.com>
2025-12-01 19:35:13 +0545

    final code push

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,13 @@ import Header from '../components/header';
 export const metadata = {
   title: 'Drone Zone Nepal | Buy, Rent & Learn Drones',
   description: 'Explore high-quality drones, accessories, and FPV gear in Nepal. Buy, rent, or learn to fly with expert training and premium branded drones at the best prices.',
+  icons: {
+    icon: [
+      { url: "/favicon-light.png", type: "image/png", media: "(prefers-color-scheme: light)" },
+      { url: "/favicon-dark.png", type: "image/png", media: "(prefers-color-scheme: dark)" },
+      { url: "/favicon.ico", type: "image/x-icon" }
+    ],
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
