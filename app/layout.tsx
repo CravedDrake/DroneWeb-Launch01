5b0b9edaf3e80ddf7646f9810cd9bd715c275674
SunimThapa <thapasunim@gmail.com>
2025-12-01 17:21:14 +0545

    Change main background color from gray-300 to gray-50

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body className="min-h-screen text-gray-800">
         <Header />
-        <main className="max-w-7xl mx-auto p-6 bg-gray-300/80">
+        <main className="max-w-7xl mx-auto p-6 bg-gray-50/95">
           {children}
         </main>
         <Footer />
